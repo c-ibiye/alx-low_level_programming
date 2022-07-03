6692cd85d169b201f314e923ba1452c322ab38a8
Ibiye Charles <c.ibiye@gmail.com>
2022-07-03 09:31:56 -0700

    7-print_tebahpla.c added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the lowercase alphabet
+ * in reverse
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'z'; ch => 'a'; ch--)
+{
+	putchar(ch);
+}
+putchar(10);
+
+return (0);
+}
