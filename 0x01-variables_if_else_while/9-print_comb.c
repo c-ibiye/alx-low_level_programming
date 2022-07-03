c66c16e3bf5b4d8b0b5fdef14ad2bb004df05b70
Ibiye Charles <c.ibiye@gmail.com>
2022-07-03 10:45:52 -0700

    9-print_comb.c updated

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 {
 int num;
 
-for (num = '0'; num <= '9'; num++)
+for (num = 48; num <= 57; num++)
 {
 	putchar(num);
-	if (num != 9)
+	if (num != 57)
 	{
 		putchar(44);
 		putchar(32);
