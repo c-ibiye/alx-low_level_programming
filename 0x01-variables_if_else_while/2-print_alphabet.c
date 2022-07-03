a00f99cfa0365969a2acc19ab6f338ee1545719d
Ibiye Charles <c.ibiye@gmail.com>
2022-07-03 06:06:42 -0700

    2-print_alphabet.c added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - ENtry point
+ *
+ * Description: This program prints alphabet in lowercase
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar("ch\n");
+}
+
+return (0);
+}
