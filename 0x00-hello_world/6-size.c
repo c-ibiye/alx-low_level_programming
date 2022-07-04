22c303da4e18beac3ce269caf8cf0778b8eaa322
Ibiye Charles <c.ibiye@gmail.com>
2022-07-04 08:57:11 -0700

    6-size.c updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ printf("Size of int: %d byte(s)\n", sizeof(int));
 printf("Size of long int: %d byte(s)\n", sizeof(long int));
 printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of float: %d byte(s)\n", sizeof(float));
-printf(stderr, "Anything");
+printf("Anything");
 return (0);
 }
