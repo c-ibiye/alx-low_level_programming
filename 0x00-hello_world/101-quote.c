ed227cc0baa15f5293ae13205f1793e9b60e32bb
Ibiye Charles <c.ibiye@gmail.com>
2022-07-04 09:49:18 -0700

    101-quote.c updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,6 @@
  */
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-fprintf(stderr, "");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
