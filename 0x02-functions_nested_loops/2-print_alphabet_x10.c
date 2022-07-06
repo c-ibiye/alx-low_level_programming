7b2ea661512ffe280683c520bc2cebfb5c003bef
Ibiye Charles <c.ibiye@gmail.com>
2022-07-06 12:44:42 -0700

    2-print_alphabet_x10.c added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+int j;
+
+while (j <= 10)
+{
+	print_alphabet();
+}
+_putchar('\n');
+}
