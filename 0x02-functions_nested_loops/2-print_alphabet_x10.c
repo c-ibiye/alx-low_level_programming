6d07447cc2a75f5048eadd33041ac0c6e1324338
Ibiye Charles <c.ibiye@gmail.com>
2022-07-06 14:08:54 -0700

    fixed coding style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,15 @@
 
 void print_alphabet_x10(void)
 {
-	int c;
-	int x;
-	for (x = 0; x < 10; x++)
+int c;
+int x;
+
+for (x = 0; x < 10; x++)
+{
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
 		_putchar(c);
-		}
+	}
 	_putchar('\n');
- 	}
+}
 }
