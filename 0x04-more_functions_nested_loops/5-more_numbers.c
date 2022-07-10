29c487f7b383f073acdd9ca644cdd446ae33a540
Ibiye Charles <c.ibiye@gmail.com>
2022-07-10 13:59:02 -0700

    5-more_numbers.c fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,18 @@
 
 void more_numbers(void)
 {
-	int m, n;
+	int m;
+	int n;
 
 	for (m = 0; m < 10; m++)
 	{
 		for (n =  0; n < 15; n++)
 		{
-			_putchar(n + '0');
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
+
 }
