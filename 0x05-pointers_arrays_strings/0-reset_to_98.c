f08a2bd68bfc5b809cbd1f3bf9c50061a6df1b9d
Ibiye Charles <c.ibiye@gmail.com>
2022-07-12 15:03:22 -0700

    fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @*n: interger param to update
+ * @n: interger param to update
  *
  * Return: nothing
  */
