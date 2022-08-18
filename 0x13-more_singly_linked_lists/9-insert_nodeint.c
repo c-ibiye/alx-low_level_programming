526ef702f3af5d9d0b7a13720b5079635322eb87
Ibiye Charles <c.ibiye@gmail.com>
2022-08-18 06:33:22 -0700

    hot fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,8 +11,8 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 1;
-
 	listint_t *new_node, *temp;
+
 	if (head == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
